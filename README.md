065b228243522ab8255e5c231c726cb636702a1b
Brodie Abrew <brodieabrew@u.boisestate.edu>
2024-10-30 18:33:14 -0600

    Finish lab

--- README.md
+++ README.md
@@ -1,24 +1,22 @@
-# Web Dev Starter Code
+# CS408 Lab 11
 
 ## Overview
 
-TODO: Write a project description
+This project makes use of an AWS server with a database to store and retrive information. A simple inventory application is implemented to demonstrate how this would be used.
+
+## Usage
+From within the project directory you can open the project using:
+
+`code .`
+
+Open "index.html" from the file explorer window and use the
+Visual Studio Code command palette (F1) to run the Live Preview Server.
 
 ## Sources and Credits
 
-TODO: You must credit the sources and authors of any code, libraries, or other
-assets you use in your project. If you leave this section blank, your project
-will be considered in violation of the Academic Honesty policy unless you truly
-created everything from scratch with no outside help. If you need to use a
-source that you cannot credit (e.g. a classmate's work), you must get explicit
-permission from your instructor.
-
-A simple bulleted list below is sufficient. For example:
-
-- Bootstrap: https://getbootstrap.com/
-- jQuery: https://jquery.com/
-- Background image: https://unsplash.com/photos/...
-- Sound effects: https://freesound.org/people/...
-- Icons: https://fontawesome.com/
-- Fonts: https://fonts.google.com/
-- etc.
+* MDN
+    * [Introduction to web APIs](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Introduction)
+    * [Fetching data from the server](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Fetching_data)
+    * [XMLHttpRequest API](https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest_API)
+    * [How to structure a web form](https://developer.mozilla.org/en-US/docs/Learn/Forms/How_to_structure_a_web_form)
+    * [CSS reference](https://developer.mozilla.org/en-US/docs/Web/CSS/Reference)
--- js/main.js
+++ js/main.js
@@ -1,18 +1,69 @@
-window.onload = loaded;
+const dataForm = document.getElementById("send-data");
+const loadData = document.getElementById("load-data");
+const tableData = document.getElementById("table-data");
 
-/**
- * Simple Function that will be run when the browser is finished loading.
- */
-function loaded() {
-    // Assign to a variable so we can set a breakpoint in the debugger!
-    const hello = sayHello();
-    console.log(hello);
+function submitDataForm(event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const xhr = new XMLHttpRequest();
+    xhr.open("PUT", "https://83wvrq58ja.execute-api.us-east-2.amazonaws.com/items");
+    xhr.setRequestHeader("Content-Type", "application/json");
+    xhr.send(JSON.stringify(Object.fromEntries(formData)));
+
+    this.reset();
 }
 
-/**
- * This function returns the string 'hello'
- * @return {string} the string hello
- */
-export function sayHello() {
-    return 'hello';
+function loadDataClicked(event) {
+    event.preventDefault();
+
+    let xhr = new XMLHttpRequest();
+    xhr.addEventListener("load", function() {
+        let allData = JSON.parse(xhr.response);
+
+        const tbodyData = tableData.children[1];
+
+        /* Not efficient at all, but it works */
+        while(tbodyData.children[0]) {
+            tbodyData.removeChild(tbodyData.children[0]);
+        }
+
+        for(let data of allData) {
+            let tableRow = document.createElement("tr");
+            
+            let tableDataID = document.createElement("td");
+            tableDataID.innerHTML = data.id;
+            tableRow.appendChild(tableDataID);
+
+            let tableDataName = document.createElement("td");
+            tableDataName.innerHTML = data.name;
+            tableRow.appendChild(tableDataName);
+
+            let tableDataPrice = document.createElement("td");
+            tableDataPrice.innerHTML = data.price;
+            tableRow.appendChild(tableDataPrice);
+
+            let tableDataDelete = document.createElement("td");
+            let deleteButton = document.createElement("button");
+            deleteButton.innerHTML = "Delete";
+            deleteButton.addEventListener("click", function() {
+                tbodyData.removeChild(tableRow);
+                xhr = new XMLHttpRequest();
+                xhr.open("DELETE", "https://83wvrq58ja.execute-api.us-east-2.amazonaws.com/items/" + data.id);
+                xhr.setRequestHeader("Content-Type", "application/json");
+                xhr.send();
+            });
+
+            tableDataDelete.classList.add("delete-button")
+            tableDataDelete.appendChild(deleteButton);
+            tableRow.appendChild(tableDataDelete);
+            tbodyData.appendChild(tableRow);
+        }
+    });
+
+    xhr.open("GET", "https://83wvrq58ja.execute-api.us-east-2.amazonaws.com/items");
+    xhr.send();
 }
+
+dataForm.addEventListener("submit", submitDataForm);
+loadData.addEventListener("click", loadDataClicked);
